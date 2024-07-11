e7e990c14fb802206460bd76303b1c4ce626dc8a
wiseaidev <oss@wiseai.dev>
2024-07-11 09:19:27 +0300

    feat: export program idl into crate

---
+++ examples/todo-program-interface/src/lib.rs
@@ -0,0 +1,7 @@
+solana_program::declare_id!("A7LQKx8zxi4smRhViytbVTksJyR1WnLqKUqua5h3SXQA");
+pub mod accounts;
+pub use accounts::*;
+pub mod instructions;
+pub use instructions::*;
+pub mod errors;
+pub use errors::*;
